f7f018391df90c949eb0a4fa8d6517056cf4c2b5
Florian Posdziech <hallo@flowfx.de>
2022-03-02 07:29:22 +0100

    Require Movement.name

--- test/models/movement_test.rb
+++ test/models/movement_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class MovementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @movement = Movement.new(name: 'Pull-up', abbreviation: 'PLU')
+  end
+
+  test 'valid movement' do
+    assert @movement.valid?
+  end
+
+  test 'invalid without name' do
+    @movement.name = nil
+
+    refute @movement.valid?
+    assert_not_nil @movement.errors[:name]
+  end
 end
