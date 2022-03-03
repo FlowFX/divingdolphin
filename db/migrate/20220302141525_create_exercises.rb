f7488f10360eed21f103448dad46047d6b27d5ef
Florian Posdziech <hallo@flowfx.de>
2022-03-03 08:55:26 +0100

    Remove sets requirements from exercise

--- db/migrate/20220302141525_create_exercises.rb
+++ db/migrate/20220302141525_create_exercises.rb
@@ -2,7 +2,7 @@ class CreateExercises < ActiveRecord::Migration[7.0]
   def change
     create_table :exercises do |t|
       t.references :movement, null: false, foreign_key: true
-      t.integer :sets, null: false, default: 1
+      t.integer :sets
       t.integer :repetitions
 
       t.timestamps
