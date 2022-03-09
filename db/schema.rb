cc13b5168b1903c1e3ce4e3930cffdd41d9e899f
Florian Posdziech <hallo@flowfx.de>
2022-03-09 19:58:37 +0100

    Rebuild schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_08_055714) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_09_184729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,7 +37,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_055714) do
     t.date "date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["exercise_id"], name: "index_performances_on_exercise_id"
+    t.index ["user_id"], name: "index_performances_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -59,4 +61,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_055714) do
 
   add_foreign_key "exercises", "movements"
   add_foreign_key "performances", "exercises"
+  add_foreign_key "performances", "users"
 end
