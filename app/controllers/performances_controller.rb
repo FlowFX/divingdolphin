c2aa839feb78c20cf5234f3757efccdc63ad48b3
Florian Posdziech <hallo@flowfx.de>
2022-03-09 20:30:03 +0100

    Add red spec that requires a form
    -- waiting for simple_form now --

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -2,4 +2,8 @@ class PerformancesController < ApplicationController
   def index
     @performances = Performance.all
   end
+
+  def new
+    @performance = Performance.new
+  end
 end
--- test/integration/performance_create_flow_test.rb
+++ test/integration/performance_create_flow_test.rb
@@ -9,4 +9,11 @@ class PerformanceCreateFlowTest < ActionDispatch::IntegrationTest
     get '/performances'
     assert_response :success
   end
+
+  test 'can create new performance' do
+    get '/performances/new'
+    assert_response :success
+
+    # TODO: continue with https://guides.rubyonrails.org/testing.html#creating-articles-integration
+  end
 end
