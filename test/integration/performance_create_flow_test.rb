93332a32b2b0df39988579b645357b28e7d51f6f
Florian Posdziech <hallo@flowfx.de>
2022-03-09 20:15:29 +0100

    Use test helper to sign in user

--- test/integration/performance_create_flow_test.rb
+++ test/integration/performance_create_flow_test.rb
@@ -1,12 +1,11 @@
 require 'test_helper'
 
 class PerformanceCreateFlowTest < ActionDispatch::IntegrationTest
-  test 'can see the performances index' do
-    post '/users/sign_in', params: { user: {
-      email: users(:kathrin).email,
-      password: 'password!!'
-    } }
+  def setup
+    sign_in users(:kathrin)
+  end
 
+  test 'can see the performances index' do
     get '/performances'
     assert_response :success
   end
