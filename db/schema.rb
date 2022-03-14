4e7512900b44d9f6ba93106e06af720c096a3f38
Florian Posdziech <hallo@flowfx.de>
2022-03-14 19:35:49 +0100

    Add unique index for exercises

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_13_080241) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_14_181217) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_13_080241) do
     t.integer "repetitions"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["movement_id", "repetitions", "sets"], name: "index_exercises_on_movement_id_and_repetitions_and_sets", unique: true
     t.index ["movement_id"], name: "index_exercises_on_movement_id"
   end
 
